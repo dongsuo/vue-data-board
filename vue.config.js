2c90dc1d770e8c3be4a901d22534f6eeb72fac9d
xuxiaofei.666 <xuxiaofei.666@bytedance.com>
2020-02-28 00:55:36 +0800

    update: build

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ function resolve(dir) {
 module.exports = {
   publicPath: '/',
   outputDir: 'docs',
+  productionSourceMap: false,
   chainWebpack(config) {
     // set svg-sprite-loader
     config.module
@@ -24,5 +25,28 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
+    config
+      .optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial' // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: 'chunk-elementUI', // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+          },
+          ECharts: {
+            name: 'chunk-ECharts',
+            priority: 20,
+            test: /[\\/]node_modules[\\/]_?echarts(.*)/ // in order to adapt to cnpm
+          }
+        }
+      })
+    // config.optimization.runtimeChunk('single')
   }
 }
